d213d741ac16d584a9d437a13458046ded30920e
TorokDan <torokdan@gmail.com>
2022-09-05 16:44:01 +0200

    deleting everything i wrote because i was mad....

--- springadvanced/src/main/java/com/gfa/springadvanced/SpringadvancedApplication.java
+++ springadvanced/src/main/java/com/gfa/springadvanced/SpringadvancedApplication.java
@@ -7,19 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @SpringBootApplication
 public class SpringadvancedApplication {
-
-    public static final String BASE_URL = "https://api.themoviedb.org/3/movie/550'";
-
     public static void main(String[] args) {
-
         SpringApplication.run(SpringadvancedApplication.class, args);
-
-        // not sure if this is in the good place...
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
     }
 
 }

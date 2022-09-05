3718edb5785daeb14cad52e649b680aa7ac58245
TorokDan <torokdan@gmail.com>
2022-09-05 14:56:10 +0200

    initial commit for the module.

---
+++ springadvanced/src/main/java/com/gfa/springadvanced/SpringadvancedApplication.java
@@ -0,0 +1,13 @@
+package com.gfa.springadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringadvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringadvancedApplication.class, args);
+    }
+
+}
